a2972ae284727a966fc5d3446f8d9546590794b8
Mike Smith <grimbough@gmail.com>
2016-07-05 11:04:08 +0200

    added swtich for building packages on server

--- calculateDependencies.R
+++ calculateDependencies.R
@@ -1,3 +1,5 @@
+LOCAL <- TRUE
+
 # read package requirements from file
 pkgs = readLines("info/packages.txt")
 # remove empty lines
@@ -17,6 +19,7 @@ pkgs = unique( c( pkgsBuild, pkgs ) )
 
 
 ### build packages in current repository
+if(!LOCAL) {
 buildPackages = FALSE
 if( buildPackages ){
   for(i in pkgsBuild){
@@ -30,6 +33,21 @@ if( buildPackages ){
 source("http://bioconductor.org/biocLite.R")
 options(repos="http://cran.r-mirror.de")
 options(BioC_mirror="http://bioconductor.statistik.tu-dortmund.de")  
+} else {
+buildPackages = TRUE
+if( buildPackages ){
+  for(i in pkgsBuild){
+    system( sprintf("cd /home/csama/packages/rsync/3.3/bioc/src/contrib/ && R CMD build /home/csama/Bressanone2016/InstallationScriptsCSAMA2016/additionalPackages/src/contrib/%s", i) )
+  }
+  write_PACKAGES("/home/csama/packages/rsync/3.3/bioc/src/contrib", verbose=TRUE, type="source")
+  write_PACKAGES("/home/csama/packages/rsync/3.3/bioc/bin/windows/contrib/3.3", verbose=TRUE, type="win.binary")
+  write_PACKAGES("/home/csama/packages/rsync/3.3/bioc/bin/macosx/mavericks/contrib/3.3/", verbose=TRUE, type="mac.binary")
+}
+
+source("http://bioconductor.org/biocLite.R")
+options(repos="http://cran.r-mirror.de")
+options(BioC_mirror="http://bioconductor.statistik.tu-dortmund.de") 
+}
 #
 repList <- biocinstallRepos()
 #
@@ -57,11 +75,11 @@ deps <- setdiff(deps, c("Rmpi"))
 #
 deps <- paste0('c("',paste(deps, collapse='", "'), '")')
 # load templates
-online = readLines("./installPackages.template")
-#server = readLines("../server/public_html/installPackages.template")
+#online = readLines("./installPackages.template")
+server = readLines("./installPackages_local.template")
 # inject list of required packages
-online = sub("DEPS", deps, online)
-# server = sub("DEPS", deps, server)
+#online = sub("DEPS", deps, online)
+server = sub("DEPS", deps, server)
 # write ready-to-use script files
-cat(online, file="./installPackages.R", sep="\n")
-#cat(server, file="../server/public_html/installPackages.R", sep="\n")
+#cat(online, file="./installPackages.R", sep="\n")
+cat(server, file="/home/csama/Bressanone2016/server/public_html/installPackages.R", sep="\n")
