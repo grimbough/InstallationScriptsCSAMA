876dd38de1e87ffcf337c960ded0531e92707562
Mike Smith <grimbough@gmail.com>
2018-06-19 11:22:20 +0200

    updating references to 2017

--- server_setup/wget_repositories.sh
+++ server_setup/wget_repositories.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 wget="wget -r -np -l 1 -A gz,zip,tgz -erobots=off"
-R_ver=3.4
-Bioc_ver=3.5
+R_ver=3.5
+Bioc_ver=3.7
 #
 # Bioconductor
 #
@@ -10,23 +10,21 @@ Bioc_ver=3.5
 
 $wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/bioc/src/contrib/
 $wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/bioc/bin/windows/contrib/$R_ver/
-$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/bioc/bin/macosx/contrib/$R_ver/
-$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/bioc/bin/macosx/mavericks/contrib/$R_ver/
+$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/bioc/bin/macosx/el-capitan/contrib/$R_ver/
 
 $wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/data/annotation/src/contrib/
 $wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/data/annotation/bin/windows/contrib/$R_ver/
-$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/data/annotation/bin/macosx/contrib/$R_ver/
-$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/data/annotation/bin/macosx/mavericks/contrib/$R_ver/
+$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/data/annotation/bin/macosx/el-capitan/contrib/$R_ver/
 
 $wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/data/experiment/src/contrib/
 $wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/data/experiment/bin/windows/contrib/$R_ver/
-$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/data/experiment/bin/macosx/contrib/$R_ver/
-$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/data/experiment/bin/macosx/mavericks/contrib/$R_ver/
+$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/data/experiment/bin/macosx/el-capitan/contrib/$R_ver/
 
-$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/extra/src/contrib/
-$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/extra/bin/windows/contrib/$R_ver/
-$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/extra/bin/macosx/contrib/$R_ver/
-$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/extra/bin/macosx/mavericks/contrib/$R_ver/
+# no longer required
+#$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/extra/src/contrib/
+#$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/extra/bin/windows/contrib/$R_ver/
+#$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/extra/bin/macosx/contrib/$R_ver/
+#$wget http://bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver/extra/bin/macosx/mavericks/contrib/$R_ver/
 
 mv bioconductor.statistik.tu-dortmund.de/packages/$Bioc_ver $HOME/R_packages/bioc_$Bioc_ver
 
