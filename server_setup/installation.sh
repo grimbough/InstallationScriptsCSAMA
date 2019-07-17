ce7c201a5c5480c133cab04387f8493f6202352e
Mike Smith <grimbough@gmail.com>
2019-07-17 11:01:56 +0200

    Homepage styling

--- server_setup/installation.sh
+++ server_setup/installation.sh
@@ -27,3 +27,27 @@ $REPO_HOME/server_setup/rsync_repositories.sh
 
 ## install R
 sudo echo 'deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/' >> /etc/apt/sources.list.d/additional-repositories.list
+
+
+
+## share internet connection
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo iptables -A FORWARD -o wlp4s0 -i enx0050b61fb9aa -s 192.168.0.0/24 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -t nat -F POSTROUTING
+sudo iptables -t nat -A POSTROUTING -o wlp4s0 -j MASQUERADE
+
+sudo iptables -I FORWARD -o wlp4s0 -s 192.168.0.0/24 -j ACCEPT
+sudo iptables -I INPUT -s 192.168.0.0/24 -j ACCEPT
+
+
+## clean IPTABLES
+
+sudo iptables --policy INPUT ACCEPT;
+sudo iptables --policy OUTPUT ACCEPT;
+sudo iptables --policy FORWARD ACCEPT;
+
+sudo iptables -Z;
+sudo iptables -F;
+sudo iptables -X;
+
